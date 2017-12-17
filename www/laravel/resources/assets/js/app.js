a85fd55a7779c76dbd2a7b0292e39eba71c3d81c
kohekondo <1211lta@gmail.com>
2017-12-17 16:01:23 +0900

    リンクの2までやった

--- www/laravel/resources/assets/js/app.js
+++ www/laravel/resources/assets/js/app.js
@@ -1,22 +1,50 @@
+//
+///**
+// * First we will load all of this project's JavaScript dependencies which
+// * includes Vue and other libraries. It is a great starting point when
+// * building robust, powerful web applications using Vue and Laravel.
+// */
+//
+//require('./bootstrap');
+//
+//window.Vue = require('vue');
+//
+///**
+// * Next, we will create a fresh Vue application instance and attach it to
+// * the page. Then, you may begin adding components to this application
+// * or customize the JavaScript scaffolding to fit your unique needs.
+// */
+//
+//Vue.component('example', require('./components/ExampleComponent.vue'));
+//
+//const app = new Vue({
+//    el: '#app'
+//});
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 require('./bootstrap');
 
-window.Vue = require('vue');
+Vue.use(VueRouter);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
+Vue.component('nav-footer', require('./components/NavFooter.vue'));
 
-Vue.component('example', require('./components/ExampleComponent.vue'));
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        { path: '/', component: require('./components/ExampleComponent.vue') },
+        { path: '/example', component: require('./components/Example.vue') },
+        { path: '/example/1', component: require('./components/Example1.vue') },
+        { path: '/example/2', component: require('./components/Example2.vue') },
+    ]
+});
 
 const app = new Vue({
+    router,
     el: '#app'
 });
--- www/laravel/resources/views/app.blade.php
+++ www/laravel/resources/views/app.blade.php
@@ -16,10 +16,52 @@
         </script>
     </head>
     <body>
-        <div id="app">
-          <example></example>
+        <div id="app" class="container">
+            <router-view></router-view>
+            <nav-footer></nav-footer>
         </div>
     </body>
 
     <script src="js/app.js"></script>
 </html>
+
+{{-- 
+
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>SPA-samples</title>
+
+<link rel="stylesheet" href="{{ mix('css/app.css') }}"></script>
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <script>
+  window.Laravel = {
+    csrfToken: "{{ csrf_token() }}"
+  };
+  </script>
+</head>
+<body>
+  <div id="app" class="container">
+    <div class="header">
+      {{$description = ('messages.site-description') }}
+      <navbar description="{{$description}}"></navbar>
+    </div>
+    <div class="wrapper">
+      <div class="main">
+        <h2>Contetns</h2>
+        <hr>
+        <router-view price="{{('labels.price')}}"></router-view>
+      </div>
+    </div>
+    <div class="footer">
+      <nav-footer></nav-footer>
+    </div>
+  </div>
+</body>
+<script src="{{ mix('js/app.js') }}"></script>
+</html>
+--}}
